a1057da3b46dce288797b4b52175a474d4266eda
jpe0824 <jason.e24@gmail.com>
2023-10-24 17:35:53 -0600

    FIX

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import Landing from "./pages/landing";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
